87fce7ad712197657262767ebc27dfc5ea6b825a
anonymous770 <aslammiya5257@gmail.com>
2021-08-09 21:09:02 +0000

    better

--- inner_setup.sh
+++ inner_setup.sh
@@ -26,34 +26,6 @@ package() {
     sudo apt-get clean
 }
 
-chromium() {
-    echo -e "${R} [${W}-${R}]${C} Uninstalling OLD chromium..."${W}
-    chrome=(chromium* chromium-browser* snapd)
-    for hula in "${chrome[@]}"; do
-        type -p "$hula" &>/dev/null || {
-            echo -e "\n${R} [${W}-${R}]${G} Purging package : ${Y}$hula${C}"${W}
-            apt purge "$hula" -y && apt autoremove -y
-            sudo apt purge "$hula" -y && sudo apt autoremove -y
-        }
-    done
-    sudo apt update -y
-    sudo apt upgrade -y
-    sudo apt install software-properties-common gnupg --no-install-recommends -y
-    banner
-    echo -e "${R} [${W}-${R}]${C} Installing Chromium..."${W}
-    sudo echo "deb http://ftp.debian.org/debian buster main
-deb http://ftp.debian.org/debian buster-updates main" >> /etc/apt/sources.list
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
-    sudo apt update -y
-    sudo apt install chromium -y
-    sudo sed -i 's/chromium %U/chromium --no-sandbox %U/g' /usr/share/applications/chromium.desktop
-    sudo apt-get upgrade -y
-}
-
 theme() {
     theme=(Bright Xfce-flat Daloa Xfce-kde2 Xfce-kolors Xfce-4.4 Xfce-light Xfce-4.6 Xfce-orange Emacs Xfce-b5 Xfce-redmondxp Xfce-basic Xfce-saltlake Moheli Xfce-cadmium Xfce-smooth Xfce-curve Xfce-stellar Retro Xfce-dawn Xfce-winter Smoke Xfce-dusk)
     for rmi in "${theme[@]}"; do
@@ -79,20 +51,6 @@ refs() {
     sudo apt autoremove -y
     banner
     echo -e "${R} [${W}-${R}]${C} Installing Visual Studio..."${W}
-
-    # wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    # sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-    # sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-    # sudo apt install apt-transport-https -y
-    # sudo apt update -y
-    # sudo apt install code -y
-    
-    echo -e "${R} [${W}-${R}]${C} Installing Sublime text..."${W}
-    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-    sudo apt-get install apt-transport-https
-    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-    sudo apt-get update
-    sudo apt-get install sublime-text
     
     git clone --depth=1 https://github.com/vinceliuice/Layan-gtk-theme.git $HOME/Layan-gtk-theme
     sudo chmod +x $HOME/Layan-gtk-theme/install.sh
@@ -135,7 +93,6 @@ note() {
 }
 
 package
-chromium
 theme
 font
 refs
